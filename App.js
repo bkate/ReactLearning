675cab3482d3c1145d6efc73d07f851957877948
kbedda <kbedda@thoughtworks.com>
2016-10-11 17:46:55 +0200

    deal with refs in react + component composit

--- App.js
+++ App.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React from "react";
+import ReactDom from "react-dom";
 class App extends React.Component {
-  render() {
-    return React.createElement('h1',null,'hello element')
-  }
+    constructor(){
+        super();
+        this.state = {
+            red: '0',
+            green: '0',
+            blue: '0'
+        };
+        this.update = this.update.bind(this);
+    };
+
+    update(e) {
+        this.setState({
+            red: ReactDom.findDOMNode(this.refs.red.refs.inp).value,
+            green: ReactDom.findDOMNode(this.refs.green.refs.inp).value,
+            blue: ReactDom.findDOMNode(this.refs.blue.refs.inp).value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Slider ref="red" update={this.update}/>
+                {this.state.red}
+                <br/>
+                <Slider ref="green" update={this.update}/>
+                {this.state.green}
+                <br/>
+                <Slider ref="blue" update={this.update}/>
+                {this.state.blue}
+                <br/>
+            </div>
+        )
+    }
 }
+
+class Slider extends React.Component {
+    render(){
+        return(
+            <div>
+            <input ref="inp" type="range"
+                   min="0"
+                   max="255"
+                   onChange={this.props.update}/>
+            </div>
+        )
+    }
+}
+
 
 // const App = () => <h1>hello</h1>
 export default App
