7fd878160d646e82568ba51178f76a91736c4c75
kbedda <kbedda@thoughtworks.com>
2016-10-27 18:29:16 +0200

    setstateHandler and setState : tutorialspoint

--- App.js
+++ App.js
@@ -1,31 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            data : []
+        };
+        this.setStateHandler = this.setStateHandler.bind(this);
+    }
 
-    render() {
+
+    setStateHandler(){
+        var item = "this is an item";
+        var myArray = this.state.data;
+        myArray.push(item);
+        this.setState({data: myArray});
+    };
+
+     render() {
         return(
             <div>
-                <h3>Array: {this.props.propArray}</h3>
-                <h3>Bool: {this.props.propBool? "this is appearing because the bool is True..." : "this is appearing because the bool is False..."}</h3>
-                <h3>Number: {this.props.propNumber}</h3>
-                <h3>String: {this.props.propString}</h3>
+                <button onClick={this.setStateHandler}>SET HANDLER</button>
+                <h4>State Array: {this.state.data}</h4>
             </div>
         );
     }
 }
-
-App.propTypes = {
-    propArray: React.PropTypes.array.isRequired,
-    propBool: React.PropTypes.bool.isRequired,
-    propNumber: React.PropTypes.number.isRequired,
-    propString: React.PropTypes.string.isRequired
-};
-
-App.defaultProps = {
-    propArray : [1, 2, 3, 4],
-    propBool: true,
-    propNumber: 12,
-    propString: "this is my String"
-};
 
 export default App
