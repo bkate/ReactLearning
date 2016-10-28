440a9fe3af6d04ed5082713f71c39895e105c835
kbedda <kbedda@thoughtworks.com>
2016-10-28 15:27:08 +0200

    find element using reactDom : tutorialspoint

--- App.js
+++ App.js
@@ -3,19 +3,18 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
     constructor(){
         super();
-        this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
-    }
-
-
-    forceUpdateHandler(){
-        this.forceUpdate();
+        this.findMyDivNodeHandler = this.findMyDivNodeHandler.bind(this);
+    };
+    findMyDivNodeHandler(){
+        var myDiv = document.getElementById("myDiv");
+        ReactDOM.findDOMNode(myDiv).style.color = 'red';
     };
 
      render() {
         return(
             <div>
-                <button onClick={this.forceUpdateHandler}>FORCE UPDATE</button>
-                <h4>State Number: {Math.random()}</h4>
+                <button onClick={this.findMyDivNodeHandler}>FORCE UPDATE</button>
+                <div id="myDiv">NODE</div>
             </div>
         );
     }
