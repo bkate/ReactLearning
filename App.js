e1a73388c4165fe354a21505f260eaf31fcc33d1
kbedda <kbedda@thoughtworks.com>
2017-01-04 10:24:54 +0100

    React: use of router based on example from: https://github.com/reactjs/react-router-tutorial/tree/master/lessons/02-rendering-a-route

--- App.js
+++ App.js
@@ -1,55 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router'
 
-// React keys are useful when working with dynamically created components or when your lists are altered by users.
-// Setting the key value will keep your components uniquely identified after the change.
 
 class App extends React.Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            data: [
-                {
-                    component: 'First...',
-                    id: 1
-                },
-                {
-                    component: 'Second...',
-                    id: 2
-                },
-                {
-                    component: 'third...',
-                    id: 3
-                }
-            ]
-        };
-
-
-    };
 
     render() {
         return (
             <div>
-                {this.state.data.map((dynamicComponent) => <Content
-                componentData = {dynamicComponent}/>)}
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+                {this.props.children}
             </div>
         );
     }
 }
-
-class Content extends React.Component {
-    render(){
-        return(
-            <div>
-                <div>{this.props.componentData.component}</div>
-                <div>{this.props.componentData.id}</div>
-            </div>
-        )
-    }
-
-}
-
-
 export default App;
+
+
--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+import { Router, Route, Link, hashHistory, IndexRoute  } from 'react-router';
 import App from './App';
-ReactDom.render(<App/>, document.getElementById('app'));
+import Home from './modules/Home';
+import About from './modules/About';
+import Contact from './modules/Contact';
+ReactDOM.render((
+    <Router history = {hashHistory}>
+            <Route path = "/" component = {App}/>
+            {/*<IndexRoute component = {App} />*/}
+            <Route path = "home" component = {Home} />
+            <Route path = "about" component = {About} />
+            <Route path = "contact" component = {Contact} />
+    </Router>
+
+), document.getElementById('app'));
